f94cf09edd3328e16fcee466a65e96f2e2babe7d
powd <powdgit@gmail.com>
2022-11-15 04:09:58 +0100

    chore(worldbuilding,all): enable simple deployment
    A Dockerfile was created for the worldbuilding backend module.
    A docker-compose file was created in the root directory
    to build all the needed images (including the worldbuilding module).
    A simple reverse proxy with nginx was set up.
    Appropriate package script modifications were added.

--- modules/worldbuilding.backend/src/excerpts/nest/excerpts.controller.ts
+++ modules/worldbuilding.backend/src/excerpts/nest/excerpts.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { ExcerptsService } from '../excerpts.service';
 
-@Controller('excerpts')
+@Controller('worldbuilding/excerpts') // TODO: move "worldbuilding/"" part to prefix / main cfg
 export class ExcerptsController {
   constructor(private readonly excerptsService: ExcerptsService) {}
 
