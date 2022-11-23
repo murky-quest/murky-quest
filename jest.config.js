f5d3dbe7d8238ded188fcdfa0d36265b2273cae0
powd <powdgit@gmail.com>
2022-11-23 18:13:31 +0100

    chore(all): automatically detect Jest config files

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,45 @@
-// TODO: Automatically list the configs by scanning the module directories,
-//       while adhering to Jest config naming conventions
+const { readdirSync, statSync } = require('fs'); // Do NOT convert to an `import` statement for Wallaby.js to work!
 
-module.exports = {
-  projects: [
-    'modules/suggestions.backend/jest.config.js',
-    'modules/suggestions.frontend/jest.config.js',
-    'modules/worldbuilding.backend/jest.config.js',
-  ],
-};
+const isDebugModeEnabled = false; // Use for checking if it does not takes too much time (important for unit testing)
+
+function displayPerformance(callback, operationMessage = undefined) {
+  const startTime = performance.now();
+  const callbackResult = callback();
+  const endTime = performance.now();
+
+  console.log(
+    `The operation${operationMessage ? ` (${operationMessage}) ` : ' '}took ${
+      endTime - startTime
+    } milliseconds`,
+  );
+
+  return callbackResult;
+}
+
+function findJestConfigFiles() {
+  const moduleDirectories = readdirSync('./modules');
+  const possibleExtensions = ['js', 'ts', 'json', 'mjs', 'cjs'];
+
+  return moduleDirectories
+    .map((moduleDirectory) => {
+      const possibleFilePaths = possibleExtensions.map(
+        (extension) => `./modules/${moduleDirectory}/jest.config.${extension}`,
+      );
+      const foundFilePath = (function statPossibleFilePaths() {
+        for (const filePath of possibleFilePaths) {
+          try {
+            statSync(filePath);
+            return filePath;
+          } catch (e) {}
+        }
+      })();
+      return foundFilePath;
+    })
+    .filter((foundFilesPath) => !!foundFilesPath);
+}
+
+const projects = isDebugModeEnabled
+  ? displayPerformance(findJestConfigFiles, 'finding Jest config files')
+  : findJestConfigFiles();
+
+module.exports = { projects };
